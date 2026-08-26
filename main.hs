7cc42739fb45ea42676d50f40ee2f3f5d5d7d159
PranaviDevireddy <158205918+PranaviDevireddy@users.noreply.github.com>
2026-08-26 15:12:47 +0530

    Updated processing_stack function

--- main.hs
+++ main.hs
@@ -1,5 +1,20 @@
 
 
+data Tree = Node String (Maybe Tree) (Maybe Tree) | Tuple (String,String)
+    deriving Show
+
+
+data Item = T Tree | Result (String,String) | Op String
+    deriving Show
+
+
+splitAtComma :: String -> (String,String)
+splitAtComma s =
+    let x = takeWhile (/= ',') s
+        y = tail (dropWhile (/= ',') s)
+    in (x,y)
+
+
 parsing :: String -> (String,[String])
 parsing line = 
     let parts = words line
@@ -7,15 +22,10 @@ parsing line =
         rhs = drop 2 parts
     in (lhs,rhs)
 
-
-data Tree = Node String Tree Tree | Tuple (String,String)
-    deriving Show
-
-
 stack = ["S"]
 
-get_rule :: String -> [(String,[String])] -> [String]
 
+get_rule :: String -> [(String,[String])] -> [String]
 get_rule symbol [] = []
 get_rule symbol ((lhs,rhs):xs) =
     if lhs == symbol
@@ -24,50 +34,65 @@ get_rule symbol ((lhs,rhs):xs) =
 
 
 create_tree :: String -> [(String,[String])] -> Tree
-create_tree symbol grammar = 
+create_tree symbol grammar =
     let rhs = get_rule symbol grammar
-    in 
-        if (head rhs == "C" || head rhs == "U" || head rhs == "KS")
-        then Node (head rhs)
-            (create_tree (rhs !! 1) grammar)
-            (create_tree (rhs !! 2) grammar)
+    in
+        if head rhs == "C" || head rhs == "U"
+        then
+            Node (head rhs)
+                 (Just (create_tree (rhs !! 1) grammar))
+                 (Just (create_tree (rhs !! 2) grammar))
+
+        else if head rhs == "KS"
+        then
+            Node (head rhs)
+                 (Just (create_tree (rhs !! 1) grammar))
+                 Nothing
         else
             let value = head rhs
-                x = [value !! 1]
-                y = [value !! 3]
-            in Tuple (x,y)
+                inside = init (tail value)
+                parts = splitAtComma inside
+            in
+                Tuple (fst parts, snd parts)
 
-
-data Item = T Tree | Result (String,String) | Op String
-    deriving Show
-
-
-process_stack :: [Item] -> (String,String)
-
-process_stack [Result x] = x
-process_stack (T (Tuple (x,y)) : xs) = process_stack (Result (x,y) : xs)
-process_stack (T (Node "C" left right) : xs) = process_stack (T left : T right : Op "C" : xs)
-process_stack (Result (x1,y1) : T right : Op "C" : xs) = process_stack (T right : Result (x1,y1) : Op "C" : xs)
-process_stack (Result (x1,y1) : Result (x2,y2) : Op "C" : xs) = process_stack (Result (x1 ++ x2, y1 ++ y2) : xs)
 
 find_cut :: (String,String) -> Maybe (String,String)
-
 find_cut (x,y) =
-    if length x /= length y then Nothing
+    if length x /= length y
+    then Nothing
     else check x y 0
 
 
 check :: String -> String -> Int -> Maybe (String,String)
-
 check x y i =
-    if i > length x
+    if i > length y
     then Nothing
     else
-        let a = take i x
-            b = drop i x
-        in if b ++ a == y
-           then Just (a,b)
-           else check x y (i + 1)
+        let before = take i y
+            after  = drop i y
+        in
+            if after ++ before == x
+            then Just (after,before)
+            else check x y (i + 1)
+            
+
+process_stack :: [Item] -> IO Bool
+
+process_stack [Result (x,y)] =
+    case find_cut (x,y) of
+        Just (a,b) -> do
+            putStrLn ("Cut: (" ++ a ++ "," ++ b ++ ")")
+            putStrLn "conjugate"
+            return True
+
+        Nothing -> do
+            putStrLn "not conjugate"
+            return False
+
+process_stack (T (Tuple (x,y)) : xs) = process_stack (Result (x,y) : xs)
+process_stack (T (Node "C" (Just left) (Just right)) : xs) = process_stack (T left : T right : Op "C" : xs)
+process_stack (Result (x1,y1) : T right : Op "C" : xs) = process_stack (T right : Result (x1,y1) : Op "C" : xs)
+process_stack (Result (x1,y1) : Result (x2,y2) : Op "C" : xs) = process_stack (Result (x2 ++ x1, y2 ++ y1) : xs)
 
 
 main :: IO ()
@@ -76,15 +101,7 @@ main = do
     let ls = lines content
     let grammar = map parsing ls
     let tree = create_tree "S" grammar
-
-    let stack = [T tree]
-
-    let pair = process_stack stack
-    let cut = find_cut pair
-
-    putStrLn ""
     print tree
-    putStrLn ""
-    putStrLn ""
-    print cut
+    is_conjugate <- process_stack [T tree]
+    print is_conjugate
    
