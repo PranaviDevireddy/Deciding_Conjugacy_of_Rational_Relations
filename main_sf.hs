ab8fb18188a1ab6dad9058fe82a154aa01a00dbe
PranaviDevireddy <158205918+PranaviDevireddy@users.noreply.github.com>
2026-08-27 10:08:12 +0530

    sumfree expression code

---
+++ main_sf.hs
@@ -0,0 +1,124 @@
+
+
+data Tree = Node String (Maybe Tree) (Maybe Tree) | Tuple (String,String)
+    deriving Show
+
+
+data Item = T Tree | Result (String,String) | Op String
+    deriving Show
+
+
+
+parsing :: String -> (String,[String])
+parsing line = 
+    let parts = words line
+        lhs = head parts
+        rhs = drop 2 parts
+    in (lhs,rhs)
+
+stack = ["S"]
+
+
+get_rule :: String -> [(String,[String])] -> [String]
+get_rule symbol [] = []
+get_rule symbol ((lhs,rhs):xs) =
+    if lhs == symbol
+    then rhs
+    else get_rule symbol xs
+
+
+split :: String -> (String,String)
+split s =
+    let x = takeWhile (/= ',') s
+        y = tail (dropWhile (/= ',') s)
+    in (x,y)
+
+
+create_tree :: String -> [(String,[String])] -> Tree
+create_tree symbol grammar =
+    let rhs = get_rule symbol grammar
+    in
+        if head rhs == "C" || head rhs == "U"
+        then
+            Node (head rhs)
+                 (Just (create_tree (rhs !! 1) grammar))
+                 (Just (create_tree (rhs !! 2) grammar))
+
+        else if head rhs == "KS"
+        then
+            Node (head rhs)
+                 (Just (create_tree (rhs !! 1) grammar))
+                 Nothing
+        else
+            let value = head rhs
+                inside = init (tail value)
+                parts = split inside
+            in
+                Tuple (fst parts, snd parts)
+
+
+find_cut :: (String,String) -> Maybe (String,String)
+find_cut (x,y) =
+    if length x /= length y
+    then Nothing
+    else check x y 0
+
+
+check :: String -> String -> Int -> Maybe (String,String)
+check x y i =
+    if i > length y
+    then Nothing
+    else
+        let before = take i y
+            after  = drop i y
+        in
+            if after ++ before == x
+            then Just (after,before)
+            else check x y (i + 1)
+            
+
+primitive_root :: (String,String) -> (String,String)
+primitive_root (u,v) =  if odd (length u) then (u,v)
+                        else checkpattern (u,v)
+
+
+selective_repeat :: Int -> String -> String
+selective_repeat 0 x = x
+selective_repeat n x = x ++ selective_repeat n-1 x
+
+primitive_root :: (String,String) -> (String,String)
+
+process_stack :: [Item] -> IO Bool
+
+process_stack [Result (x,y)] =
+    case find_cut (x,y) of
+        Just (a,b) -> do
+            putStrLn ("Cut: (" ++ a ++ "," ++ b ++ ")")
+            putStrLn "conjugate"
+            return True
+
+        Nothing -> do
+            putStrLn "not conjugate"
+            return False
+
+process_stack (T (Tuple (x,y)) : xs) = process_stack (Result (x,y) : xs)
+process_stack (T (Node "C" (Just left) (Just right)) : xs) = process_stack (T left : T right : Op "C" : xs)
+process_stack (T (Node "KS" (Just left) Nothing) : xs) = do
+                                                            x <- process_stack [T left]
+                                                            if x == False
+                                                            then return False
+                                                            else process_stack xs
+process_stack (Result (x1,y1) : T right : Op "C" : xs) = process_stack (T right : Result (x1,y1) : Op "C" : xs)
+process_stack (Result (x1,y1) : Result (x2,y2) : Op "C" : xs) = process_stack (Result (x2 ++ x1, y2 ++ y1) : xs)
+
+
+main :: IO ()
+main = do
+    content <- readFile "input.txt"
+    let ls = lines content
+    let grammar = map parsing ls
+    let tree = create_tree "S" grammar
+    print tree
+    is_conjugate <- process_stack [T tree]
+    print is_conjugate
+   
